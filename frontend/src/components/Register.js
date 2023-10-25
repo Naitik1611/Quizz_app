9bbee1f2791d914f4c90c9b94f52782cb4aea8f9
Shivani Eranjikal <eranjikal.s@gmail.com>
2023-10-25 15:25:15 +0530

    Modified Registration

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -7,22 +7,23 @@ export default function Register() {
  
     const navigate = useNavigate();
 
-    const [username, setUsername] = useState('');
+    const [fname, setfname] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [cnfPassword, setCnfPassword] = useState('');
+    const [lname, setlname] = useState('');
+
 
     const handleSignup = async (e) => {
       e.preventDefault();
       const signupData = {
-        username,
-        email,
-        password,
-        cnfPassword
+        "firstName" : fname,
+        "lastName": lname,
+        "email" : email,
+        "password": password 
       };
       
       try {
-        const res = await axios.post('http://localhost:xxxx/',signupData)
+        const res = await axios.post('http://localhost:8080/api/users/',signupData)
         console.log(res.data);
       } catch (e) {
         alert(e.message)
@@ -33,24 +34,24 @@ export default function Register() {
     return (
   <div className='login-container'>
     <div className="details-container">
-      <form title="Sign Up" className="login-form" onSubmit={handleSignup}>
+      
+    <form title="Sign Up" className="login-form" onSubmit={handleSignup}>
         <h3>Sign Up</h3>
-        <label htmlFor="username" className="inp-label">Username</label>
-        <input type="text" id="username" className="inp inp-user" placeholder='Enter Username' onChange={(e) => setUsername(e.target.value)} required></input>
+        <label htmlFor="fname" className="inp-label">First Name</label>
+        <input type="text" id="fname" className="inp inp-user" placeholder='Enter First Name' onChange={(e) => setfname(e.target.value)} required></input>
+        
+        <label htmlFor="lname" className="inp-label">Last name</label>
+        <input type="text" id="lname" className="inp inp-pass" placeholder='Enter Last Name' onChange={(e) => setlname(e.target.value)} required></input>
 
         <label htmlFor="email" className="inp-label">Email</label>
         <input type="email" id="email" className="inp inp-pass" placeholder='Enter Email Id' onChange={(e) => setEmail(e.target.value)} required></input>
-
-        <label htmlFor="password" className="inp-label">Password</label>
+<label htmlFor="password" className="inp-label">Password</label>
         <input type="password" id="password" className="inp inp-pass" placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} required></input>
-        
-        <label htmlFor="cnf-password" className="inp-label">Confirm Password</label>
-        <input type="password" id="cnf-password" className="inp inp-pass" placeholder='Confirm Password' onChange={(e) => setCnfPassword(e.target.value)} required></input>
-        
         <button type="submit" className="btn btn-primary form-btn">Create Account</button>
       </form>
-      <span className='toggle-login'>
+       <span className='toggle-login'>
       Already have an account? <button type="button" className="btn btn-primary" onClick={() => navigate("/")}>Login</button>
+      
       </span>
     </div>
 
