9b688c233de913dad4c190aea0d163842715a6f4
Shubham Bhattad <shubham.bhattad33@gmail.com>
2023-10-29 01:29:51 +0530

    Connectivity Improved
    Filter Update
    Display of quizzes update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import './style.css'
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
@@ -9,7 +10,45 @@ import Row from 'react-bootstrap/Row';
 
 export default function Home() {
  
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [quizArray, setquizArray] = useState([]);
+    const [recommendedArray, setrecommendedArray] = useState([]);
+
+    var num = 1
+    useEffect((async) => {
+        if (num===1) {
+            getQuiz();
+            getRecommended();
+            num++;
+        }
+        
+    }, []);
+
+    const getQuiz = async (e) => {
+        try {
+            const res = await axios.get('http://localhost:8080/quiz',{
+                headers: {
+                    'authorization': localStorage.getItem("token") // Setting the 'Authorization' header with the token
+                }
+            });
+            setquizArray(res.data);
+        } catch (e) {
+            alert(e.message)
+        }
+    };
+
+    const getRecommended = async (e) => {
+        try {
+            const res = await axios.get('http://localhost:8080/quiz',{
+                headers: {
+                    'authorization': localStorage.getItem("token") // Setting the 'Authorization' header with the token
+                }
+            });
+            setrecommendedArray(res.data);
+        } catch (e) {
+            alert(e.message)
+        }
+    };
 
     return (
     <div className="main-container">
@@ -21,19 +60,20 @@ export default function Home() {
 
         <div className='home-categories-container'>
             <Row className='home-head'>
-                <Col md={1} className='home-categories-label'>Categories</Col>
-                <Col md={{span:2, offset:8}}>
-                    <Button variant="primary" className="btn">See More</Button>
+                <Col md={3} className='home-categories-label'>Latest Quizzes</Col>
+                <Col md={{span:2, offset:6}}>
+                    <Button variant="primary" className="btn" onClick={() => navigate("/quiz-category")}>See More</Button>
                 </Col>
             </Row>
             <div className='home-items'>
                 <Row xs={1} md={4} className="g-4">
-                    {Array.from({ length: 4 }).map((_, idx) => (
-                        <Col key={idx}>
+                    {quizArray.slice(0,4).map((quiz) => (
+                        <Col key={quiz._id}>
                         <Card>
                             <Card.Img variant="top" src="holder.js/100px160" />
                             <Card.Body>
-                                <Card.Title>Category Name</Card.Title>
+                                <Card.Title>{ quiz.Title }</Card.Title>
+                                <Card.Text>Category: {quiz.Category}</Card.Text>
                             </Card.Body>
                         </Card>
                         </Col>
@@ -45,18 +85,15 @@ export default function Home() {
         <div className='home-recommended-container'>
             <Row className='home-head'>
                 <Col md={3} className='home-recommended-label'>Recommended for you</Col>
-                <Col md={{span:2, offset:6}}>
-                    <Button variant="primary" className="btn">See More</Button>
-                </Col>
             </Row>
             <div className='home-items'>
                 <Row xs={1} md={4} className="g-4">
-                    {Array.from({ length: 4 }).map((_, idx) => (
-                        <Col key={idx}>
+                    {recommendedArray.slice(4,8).map((quiz) => (
+                        <Col key={quiz._id}>
                         <Card>
                             <Card.Img variant="top" src="holder.js/100px160" />
                             <Card.Body>
-                                <Card.Title>Recommended Quiz</Card.Title>
+                                <Card.Title>{ quiz.Title }</Card.Title>
                             </Card.Body>
                         </Card>
                         </Col>
