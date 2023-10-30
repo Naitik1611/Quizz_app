4a9040c590e352b564a825eedbb25334b563f780
spcrsankar <sankaralingam2910@gmail.com>
2023-10-30 15:26:11 +0530

    take quiz based on user updated in attempt_quiz.js

--- server/attempt_quiz.js
+++ server/attempt_quiz.js
@@ -71,6 +71,22 @@ exports.attemptQuiz = async (quizId, userId, answers, res) => {
   exports.displayQuestions=async(req,res)=>{
     try{
       const quizId=req.params.id;
+
+      //check type of user
+      const userId = req._id
+      const user = await User.findById(userId)
+
+      const q = await Quiz.findById(quizId)
+      
+      let participated = user.scores.forEach((score)=>q._id.toString() === score.Quiz_id.toString())
+      console.log(q.Creat)
+      if(q.Creator_id){
+        if(participated)
+          return res.status(205).send({"message":"Already participated in this quiz"})
+        else if(q.Creator_id.toString() === userId.toString())
+          return res.status(206).send({"message":"Creator of the quiz cannot attempt quiz"})
+      }        
+
       //console.log(quizId);
       const quiz = await Quiz.findById(quizId).populate('Questions');
       console.log(quiz);
@@ -83,7 +99,7 @@ exports.attemptQuiz = async (quizId, userId, answers, res) => {
         Question_id:question._id,
         Question_text: question.Question_text,
         Question_type:question.Question_type,
-        Correct_answer:encrypt(question.Correct_answer, process,env.SECRET),
+        Correct_answer:encrypt(question.Correct_answer, process.env.SECRET),
         Explanation: question.Explanation,
         Score: question.Score,
         Time: question.Time,
--- server/filter.js
+++ server/filter.js
@@ -1,5 +1,4 @@
 const Quiz = require('./models/quiz');
-const {User} = require('./models/user');
 
 
 exports.filterQuizzesByCategory = async (req, res) => {
@@ -7,24 +6,6 @@ exports.filterQuizzesByCategory = async (req, res) => {
     const { category } = req.params; 
     let quizzes = await Quiz.find({ Category: category }); 
 
-    const userId = req._id
-    const user = await User.findById(userId)
-
-    quizzes = quizzes.map((quiz)=>{
-      let user_type = 0
-      let participated = user.scores.forEach((score)=>quiz._id.toString() === score.Quiz_id.toString())
-      console.log(quiz.Creator_id, userId)
-      if(quiz.Creator_id){
-        if(participated)
-          user_type = 1
-        else if(quiz.Creator_id.toString() === userId.toString())
-          user_type = 2
-      }        
-      return {
-        ...quiz,
-        user_type
-      }
-    })
     res.status(200).json({ quizzes });
   } catch (error) {
     console.error(error);
